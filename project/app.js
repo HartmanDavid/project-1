f1631805062b0a807fbfe2903566be5f68352112
David Hartman <david@petminded.com>
2015-07-07 11:36:38 -0700

    added all functions to each control key for both 1st and 2nd dog

--- project/app.js
+++ project/app.js
@@ -1,12 +1,18 @@
 console.log('hi');
+// var dFirst = document.querySelector("#first-dog").
 
-$('body').on("keydown", function(e){;
+$('body').on("keydown", function(e){
         if (e.which == 76){console.log('L');
-            $("#first-dog").animate({top: "15%"},200,"linear").animate({top: "30%"},400,"linear");}
-          else if (e.which == 188) { console.log(',');}
-          else if (e.which == 190) { console.log('.');}
+              $("#first-dog").animate({top: "15%"},200,"linear").animate({top: "30%"},400,"linear");}
+          else if (e.which == 188) { console.log(',');
+              document.querySelector("#first-dog");
+              $("#first-dog").animate({marginLeft: ".5em"},400,"linear").animate({marginLeft: "3em"},200,"linear");}
+          else if (e.which == 190) { console.log('.');
+              $("#first-dog").animate({marginLeft: "4.5em"},400,"linear").animate({marginLeft: "3em"},200,"linear");}
           else if (e.which == 83) { console.log('S');
-          $("#second-dog").animate({top: "45%"},200,"linear").animate({top: "60%"},400,"linear");}
-          else if (e.which == 90) { console.log('Z');}
-          else if (e.which == 88) { console.log('X');}
+              $("#second-dog").animate({top: "45%"},200,"linear").animate({top: "60%"},400,"linear");}
+          else if (e.which == 90) { console.log('Z');
+              $("#second-dog").animate({marginLeft: ".5em"},400,"linear").animate({marginLeft: "3em"},200,"linear");}
+          else if (e.which == 88) { console.log('X');
+              $("#second-dog").animate({marginLeft: "4.5em"},400,"linear").animate({marginLeft: "3em"},200,"linear");}
     });
