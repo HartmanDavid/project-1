aaf67c747023429d1e3d82db33f108716b38395c
David Hartman <david@petminded.com>
2015-07-06 18:17:17 -0700

    modified HTML, JS now detects basic 3 keys per player & console.log()

--- project/app.js
+++ project/app.js
@@ -1,1 +1,11 @@
 console.log('hi');
+
+$('body').on("keydown", function(e){console.log(e);
+        var e.which = eW
+        if (eW == 76){console.log('L');}
+          else if (eW == 188) { console.log(',');}
+          else if (eW == 190) { console.log('.');}
+          else if (eW == 83) { console.log('S');}
+          else if (eW == 90) { console.log('Z');}
+          else if (eW == 88) { console.log('X');}
+    });
