9f3fdf953b3495aa6ccb92c03f4d34bab31fca60
David Hartman <david@petminded.com>
2015-07-06 19:13:00 -0700

    fixed dog 1&2 position in CSS and jumping in JQuery

--- project/app.js
+++ project/app.js
@@ -1,10 +1,12 @@
 console.log('hi');
 
-$('body').on("keydown", function(e){console.log(e);
-        if (e.which == 76){console.log('L');}
+$('body').on("keydown", function(e){;
+        if (e.which == 76){console.log('L');
+            $("#first-dog").animate({top: "15%"},200,"linear").animate({top: "30%"},400,"linear");}
           else if (e.which == 188) { console.log(',');}
           else if (e.which == 190) { console.log('.');}
-          else if (e.which == 83) { console.log('S');}
+          else if (e.which == 83) { console.log('S');
+          $("#second-dog").animate({top: "45%"},200,"linear").animate({top: "60%"},400,"linear");}
           else if (e.which == 90) { console.log('Z');}
           else if (e.which == 88) { console.log('X');}
     });
