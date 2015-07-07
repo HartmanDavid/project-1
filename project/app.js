ebb8fa12e26f502f83169aa1d1c2aa0d92af7bff
David Hartman <david@petminded.com>
2015-07-06 18:21:27 -0700

    fixed bugs with e.which

--- project/app.js
+++ project/app.js
@@ -1,11 +1,10 @@
 console.log('hi');
 
 $('body').on("keydown", function(e){console.log(e);
-        var e.which = eW
-        if (eW == 76){console.log('L');}
-          else if (eW == 188) { console.log(',');}
-          else if (eW == 190) { console.log('.');}
-          else if (eW == 83) { console.log('S');}
-          else if (eW == 90) { console.log('Z');}
-          else if (eW == 88) { console.log('X');}
+        if (e.which == 76){console.log('L');}
+          else if (e.which == 188) { console.log(',');}
+          else if (e.which == 190) { console.log('.');}
+          else if (e.which == 83) { console.log('S');}
+          else if (e.which == 90) { console.log('Z');}
+          else if (e.which == 88) { console.log('X');}
     });
